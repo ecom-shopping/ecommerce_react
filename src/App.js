e737f040c5beb8d03f4a160337991d16be9d22af
harikrishna <harikrishna6525@gmail.com>
2022-08-18 14:44:53 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import Nav from "./Components/Nav";
-
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
@@ -9,6 +8,7 @@ function App() {
     <Router>
       <Nav />
       <div className="main">
+        <Nav />
         <Routes></Routes>
       </div>
     </Router>
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -5,35 +5,7 @@ import { NavLink}from 'react-router-dom'
 
 function Nav() {
     return (
-        // <Router>
-            <div className="navbar">
-                <ul>
-                    <li>
-                        <NavLink className='position' exact activeClassName="active" to="/">
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink className='position'  activeClassName="active" to="/create">
-                            Create
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink className='position' activeClassName="active" to="/read">
-                            Read
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink className='position' activeClassName="active" to="/update">
-                            Update
-                        </NavLink>
-                    </li>
-                </ul>
-                <hr />
-               
-
-            </div>
-        // </Router>
+      
 
 
     )
