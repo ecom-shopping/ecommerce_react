28aa2e201f72eb66a31541a6edf6eafae1bb1d95
harikrishna <harikrishna6525@gmail.com>
2022-08-18 15:52:21 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
-import Nav from "./Components/Nav";
+// import Nav from "./Components/Nav";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Nav />
+      {/* <Nav /> */}
       <div className="main">
-        <Nav />
+        {/* <Nav /> */}
         <Routes></Routes>
       </div>
     </Router>
