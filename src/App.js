a0174e56f3cd2eb2cf87efaf062164801f3befad
harikrishna <harikrishna6525@gmail.com>
2022-08-18 16:04:35 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+// import Nav from "./Components/Nav";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      {/* <Nav /> */}
+      <div className="main">
+        {/* <Nav /> */}
+        kkk
+        <Routes></Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
